15785ef57d515abc4860134ab95afd2435a5c7e8
zuochiren <2398383525@qq.com>
2018-03-22 17:13:53 +0800

    图片上传修改，同步数据库

--- server/servers.js
+++ server/servers.js
@@ -2,6 +2,7 @@ const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
 var mysql=require('mysql')
+var multer = require('multer');
 var express=require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -14,7 +15,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'blog'
+    database: 'blog8888'
 });
 connection.connect();
 /*登录后台*/
@@ -86,6 +87,62 @@ app.post("/admin-v2", function (req, res) {
         res.send(results);
     })
 })
+
+
+var src = ''
+var storage = multer.diskStorage({
+    // 上传文件夹
+    destination : function(req, file, cb){
+        cb(null, '../img/bgimg')
+    },
+    // 保存的文件名字
+    filename : function(req, file, cb){
+        var path = file.originalname
+        //上传图片
+        connection.query('INSERT INTO `blog_img` SET ?', {
+            u_path : path,
+            u_class : 1,
+        })
+        cb(null, path)
+    }
+})
+
+var upload = multer({
+    storage : storage
+});
+//预览上传图片
+app.post('/getimg', upload.any(), function(req, res){
+    res.append("Access-Control-Allow-Origin", "*");
+    connection.query('SELECT * FROM blog_img', function(error, results, fields){
+        for(var i = 0; i < results.length; i++){
+            src = results[i].u_path
+        }
+        res.end(src);
+    })
+});
+
+//获取数据库图片
+app.post('/getimages', upload.any(), function(req, res){
+    res.append("Access-Control-Allow-Origin", "*");
+    connection.query('SELECT * FROM blog_img', function(error, results, fields){
+        var str1=JSON.stringify(results)
+        res.end(str1);
+    })
+});
+
+
+//改变图片状态
+app.post('/removeimg', upload.any(),function(req, res){
+    console.log(req.body.id);
+    res.append("Access-Control-Allow-Origin", "*");
+
+    connection.query("update blog_img set ? where id='"+req.body.id+" '  ",{
+        u_class:0
+    }, function(error, results, fields){
+        res.send("成功")
+    })
+})
+
 
 app.listen(5656)
 console.log("开启服务器");
--- src/js/server.js
+++ src/js/server.js
@@ -1,6 +1,15 @@
 var express = require('express');
+const http = require('http');
 var multer = require('multer');
 var mysql = require('mysql')
+var app = express()
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended:false
+}))
+
+app.use(bodyParser.json())
+
 var connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
@@ -18,9 +27,10 @@ var storage = multer.diskStorage({
     // 保存的文件名字
     filename : function(req, file, cb){
         var path = file.originalname
-
+//上传图片
         connection.query('INSERT INTO `blog_img` SET ?', {
             u_path : path,
+            u_class : 1,
         })
         cb(null, path)
     }
@@ -29,7 +39,7 @@ var storage = multer.diskStorage({
 var upload = multer({
     storage : storage
 });
-var app = express()
+//预览上传图片
 app.post('/getimg', upload.any(), function(req, res){
     res.append("Access-Control-Allow-Origin", "*");
     connection.query('SELECT * FROM blog_img', function(error, results, fields){
@@ -39,9 +49,29 @@ app.post('/getimg', upload.any(), function(req, res){
         res.end(src);
     })
 });
+
+//获取数据库图片
+app.post('/getimages', upload.any(), function(req, res){
+    res.append("Access-Control-Allow-Origin", "*");
+    connection.query('SELECT * FROM blog_img', function(error, results, fields){
+        var str1=JSON.stringify(results)
+        res.end(str1);
+    })
+});
+
+
+//改变图片状态
+app.post('/removeimg', upload.any(),function(req, res){
+    console.log(req.body.id);
+    res.append("Access-Control-Allow-Origin", "*");
+
+    connection.query("update blog_img set ? where id='"+req.body.id+" '  ",{
+        u_class:0
+    }, function(error, results, fields){
+        res.send("成功")
+    })
+})
 app.listen(9999);
-
-
 
 
 
